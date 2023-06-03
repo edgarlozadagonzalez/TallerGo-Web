47f99d40b608de9f16dd2c2a0f2387dc6cb9c485
David Lozada <edgar.lozada@unillanos.edu.co>
2023-06-03 09:08:35 -0500

    archivo docker

--- main.go
+++ main.go
@@ -10,17 +10,17 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/", controllers.Index)
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/curso/topMejoresEstudiantes", controllers.TopMejoresEstudiantes)
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/curso/topPeoresEstudiantes", controllers.TopPeoresEstudiantes)
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/curso/calcularEstadisticas", controllers.CalcularEstadisticas)
+	http.HandleFunc("/", controllers.Index)
+	http.HandleFunc("/curso/topMejoresEstudiantes", controllers.TopMejoresEstudiantes)
+	http.HandleFunc("/curso/topPeoresEstudiantes", controllers.TopPeoresEstudiantes)
+	http.HandleFunc("/curso/calcularEstadisticas", controllers.CalcularEstadisticas)
 
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/estudiante/mejorPromedio", controllers.MejorPromedio)
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/estudiante/peorPromedio", controllers.PeorPromedio)
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/estudiante/masculinoMasLongevo", controllers.MasculinoMasLongevo)
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/estudiante/femeninaMasLongeva", controllers.FemeninaMasLongeva)
+	http.HandleFunc("/estudiante/mejorPromedio", controllers.MejorPromedio)
+	http.HandleFunc("/estudiante/peorPromedio", controllers.PeorPromedio)
+	http.HandleFunc("/estudiante/masculinoMasLongevo", controllers.MasculinoMasLongevo)
+	http.HandleFunc("/estudiante/femeninaMasLongeva", controllers.FemeninaMasLongeva)
 
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/reporteMatriculas", controllers.ReporteMatriculas)
-	http.HandleFunc("https://tallergo-web-production.up.railway.app/promedioRangoEdad", controllers.PromedioRangoEdad)
+	http.HandleFunc("/reporteMatriculas", controllers.ReporteMatriculas)
+	http.HandleFunc("/promedioRangoEdad", controllers.PromedioRangoEdad)
 	http.ListenAndServe(":8080", nil)
 }
